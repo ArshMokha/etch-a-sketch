ac5461d823d11531fcccc0d31969c995c2ad2db8
Arshdeep Mokha <arshdeepm147@gmail.com>
2024-05-26 20:20:07 -0700

    Add resizing capabilities

--- javascript.js
+++ javascript.js
@@ -1,12 +1,34 @@
-const sketchSize = prompt("What size would you like?: ", 64);
-
 const container = document.querySelector(".container");
 
-function setSize() {
-  const size = sketchSize * 16;
+const resize = document.querySelector(".sketch-size");
 
-  container.style.width = size + "px";
-  container.style.height = size + "px";
+resize.addEventListener("click", () => {
+  sketchSize = prompt("What size would you like?: ", 64);
+  if (sketchSize <= 100) {
+    setSize();
+    deletePixels();
+    createPixels();
+    createSketch();
+  } else {
+    container.innerText = "Size too large";
+  }
+})
+
+let sketchSize;
+
+function setSize() {
+    const size = sketchSize * 16;
+
+    container.style.width = size + "px";
+    container.style.height = size + "px";
+}
+
+function deletePixels() {
+  const pixels = document.querySelectorAll(".pixel");
+
+  for (let pixel of pixels) {
+    pixel.remove();
+  }
 }
 
 function createPixels() {
